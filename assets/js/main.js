f84c981e994cc69c1e50a55666aef0ce9941b238
AleffCorreia <aleffcorreia@outlook.com>
2021-09-09 11:48:12 -0300

    includes section qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ if (navToggle) {
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
+if (navClose) {
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
   });
@@ -22,7 +22,7 @@ if(navClose){
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
   const navMenu = document.getElementById('nav-menu')
 
   navMenu.classList.remove('show-menu');
@@ -35,13 +35,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const skillsContent = document.getElementsByClassName('skills__content'),
   skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
+function toggleSkills() {
   let itemClass = this.parentNode.className
 
-  for(i = 0; i < skillsContent.length; i++){
+  for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = 'skills__content skills__close'
   }
-  if(itemClass == 'skills__content skills__close'){
+  if (itemClass == 'skills__content skills__close') {
     this.parentNode.className = 'skills__content skills__open'
   }
 }
@@ -51,7 +51,24 @@ skillsHeader.forEach((el) => {
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content')
 
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active');
+    })
+    target.classList.add('qualification__active');
+
+    tab.forEach(tab => {
+      tab.classList.remove('qualification__active');
+    })
+    tab.classList.add('qualification__active');
+  });
+});
 
 /*==================== SERVICES MODAL ====================*/
 
