408153be7de10b8ba0b448a45b46e4077501a411
AleffCorreia <aleffcorreia@outlook.com>
2021-09-14 00:04:37 -0300

    includes testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.swiper-container', {
+let swiperPorfolio = new Swiper('.portfolio__container', {
   cssMode: true,
   loop: true,
   navigation: {
@@ -109,7 +109,22 @@ let swiper = new Swiper('.swiper-container', {
 })
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+    
+  },
+  breakpoints:{
+    568: {
+      slidesPerView: 2,
+    }
+  }
+})
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
